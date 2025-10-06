28b536b670c10cba3ea7d0dc6a73a3fa878bdd4f
GXeLla <g.xelashvili2001@gmail.com>
2025-10-06 19:19:56 +0200

    updated2

--- script/main.js
+++ script/main.js
@@ -1,16 +1,17 @@
-// loading bar gets hidden after X comes up 
-setTimeout(function(){
-    let preloaditms = document.querySelector('.progress-bar-main-container')
-    preloaditms.classList.add('hidden');
-},18500);
+// loading bar gets hidden after X comes up
+setTimeout(function () {
+  let preloaditms = document.querySelector(".progress-bar-main-container");
+  preloaditms.classList.add("hidden");
+}, 8500);
 
-setTimeout(function() {
-    let preloader = document.querySelector('.loading-container');
-    preloader.classList.add('hidden');
+setTimeout(function () {
+  let preloader = document.querySelector(".loading-container");
+  preloader.classList.add("hidden");
 
-    
-    // after fade animation set display none
-    setTimeout(function() {
-        preloader.style.display = 'none';
-    }, 1000); 
-}, 22000);
+  // after fade animation set display none
+  setTimeout(function () {
+    preloader.style.display = "none";
+  }, 1000);
+  });
+  // edit this with bottom!!!
+// }, 11000);
