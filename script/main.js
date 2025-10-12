07dab3c1fc8718689884279db70369ffba7e1c36
GXeLla <g.xelashvili2001@gmail.com>
2025-10-12 17:38:45 +0200

    rain added and some phone-user fixes

--- script/main.js
+++ script/main.js
@@ -12,6 +12,30 @@ setTimeout(function () {
   setTimeout(function () {
     preloader.style.display = "none";
   }, 1000);
-  }, 11000);
-  // });
-  // edit this with bottom!!!
+}, 11000);
+// });
+// edit this with bottom!!!
+
+//cloud raindrops
+function rain() {
+  let cloud = document.querySelector(".cloud");
+  let e = document.createElement("div");
+  let left = Math.floor(Math.random() * 235) + 45;
+  let width = Math.random() * 5;
+  let height = Math.random() * 50;
+  let duration = Math.floor(Math.random() * 6) + 1;
+
+  e.classList.add("drop");
+  cloud.appendChild(e);
+  e.style.left = left + "px";
+  e.style.width = 0.3 + width + "px";
+  e.style.height = 0.5 + height + "px";
+  e.style.animationDuration = duration + "s";
+
+  setTimeout(function () {
+    cloud.removeChild(e);
+  }, 2000);
+}
+setInterval(function () {
+  rain();
+}, 20);
